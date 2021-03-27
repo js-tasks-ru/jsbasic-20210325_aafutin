469be979daacc74ba129c29e8e42633b0a272a35
aafutin <alex@afut.in>
2021-03-28 02:49:16 +0300

    add solution for 1-module

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,8 @@
 function ucFirst(str) {
-  // ваш код...
+  if (typeof str !== 'string') {
+    throw new Error('str must be a string');
+  }
+  const firstLetter = str[0] && str[0].trim() || '';
+  const remainingPart = str.slice(1);
+  return firstLetter.toUpperCase() + remainingPart;
 }
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,14 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (typeof str !== 'string') {
+    throw new Error('str must be a string');
+  }
+  else if (!['number', undefined].includes(typeof maxlength)) {
+    throw new Error('maxlength must be a number');
+  }
+  else if (str.length <= maxlength) {
+    return str;
+  }
+  else {
+    return str.slice(0, --maxlength) + '…';
+  }
 }

2fd42dfba13f3b0ab88dbf39ad3acdc416fb4703
aafutin <alex@afut.in>
2021-03-28 02:52:31 +0300

    fix

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -2,7 +2,8 @@ function ucFirst(str) {
   if (typeof str !== 'string') {
     throw new Error('str must be a string');
   }
-  const firstLetter = str[0] && str[0].trim() || '';
+  str = str.trim();
+  const firstLetter = str[0];
   const remainingPart = str.slice(1);
   return firstLetter.toUpperCase() + remainingPart;
 }

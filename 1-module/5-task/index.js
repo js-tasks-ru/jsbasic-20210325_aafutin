2ff2abacdda36e6cf32c4e2c7496b3949e4b8827
aafutin <alex@afut.in>
2021-03-28 03:05:18 +0300

    fix

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -2,7 +2,7 @@ function truncate(str, maxlength) {
   if (typeof str !== 'string') {
     throw new Error('str must be a string');
   }
-  else if (!['number', undefined].includes(typeof maxlength)) {
+  else if (typeof maxlength !== 'number') {
     throw new Error('maxlength must be a number');
   }
   else if (str.length <= maxlength) {
